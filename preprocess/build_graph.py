9378ab52525dd79a0c06a791f495c6e569090ca2
ngoctuannguyen1980123@gmail.com <ngoctuannguyen1980123@gmail.com>
2024-04-29 22:46:22 +0700

    feat: Label list update in build_graph.py

--- preprocess/build_graph.py
+++ preprocess/build_graph.py
@@ -4,11 +4,11 @@ import numpy as np
 import pickle as pkl
 import scipy.sparse as sp
 from math import log
-from sklearn import svm
 from nltk.corpus import wordnet as wn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy.spatial.distance import cosine
 import sys
+import itertools
 
 # from utils.utils import clea
 # if len(sys.argv) != 2:
@@ -67,6 +67,7 @@ with open('data/corpus/' + dataset + '_shuffle.txt', 'w') as f:
     f.write('\n'.join(shuffle_doc_content_list))
 
 # print(shuffle_doc_content_list[0].split())
+# Build Vocabulary
 word_freq = {}
 vocab = set()
 for content in shuffle_doc_content_list:
@@ -75,4 +76,42 @@ for content in shuffle_doc_content_list:
         vocab.add(word)
         word_freq[word] = word_freq[word] + 1 if word in word_freq else 1
 
-print(len(vocab))
+vocab = list(vocab)
+
+word_doc_list = {}
+for i in range(len(shuffle_doc_content_list)):
+    doc_words = shuffle_doc_content_list[i]
+    words = doc_words.split()
+    appeared = set()
+    for word in words:
+        if word in appeared:
+            continue    
+        if word in word_doc_list:
+            word_doc_list[word].append(i)
+        else:   
+            word_doc_list[word] = [i]
+        appeared.add(word)
+
+word_doc_freq = {}
+for word, doc_list in word_doc_list.items():
+    word_doc_freq[word] = len(doc_list)
+# print(dict(itertools.islice(word_doc_freq.items(), 10)))
+
+word_id_map = {}
+for i in range(len(vocab)):
+    word_id_map[vocab[i]] = i
+
+vocab_str = '\n'.join(vocab)
+with open('data/corpus/' + dataset + '_vocab.txt', 'w') as f:
+    f.write(vocab_str)
+
+label_set = set()
+# print(shuffle_doc_name_list)
+for doc_meta in shuffle_doc_name_list:
+    temp = doc_meta.split('\t')
+    label_set.add(temp[2])
+label_list_str = '\n'.join(label_set)
+# print(label_list_str)
+with open('data/corpus/' + dataset + '_labels.txt', 'w') as f:
+    f.write(label_list_str)
+
